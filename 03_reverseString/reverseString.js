10ddc67056c559348bd48d44b66714c38c3f20f9
Josue S <68451593+HouseJoJo@users.noreply.github.com>
2023-01-02 15:51:44 -0500

    First commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arr = str.split("");
+    const out = [];
+    for(let i = arr.length; i >= 0; i--){
+        out.push(arr[i]);
+    }
+    return out.join("");
 };
 
 // Do not edit below this line
