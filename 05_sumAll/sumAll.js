76cc96c9e205182a015498f35fd7892b788133af
Josue S <68451593+HouseJoJo@users.noreply.github.com>
2023-01-02 16:51:09 -0500

    complete exercises 01-07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0) return "ERROR";
+    let bigNum = 0;
+    let smallNum = 0;
+    let sum = 0;
+    if (num1 > num2){
+        bigNum = num1;
+        smallNum = num2;
+    }
+    else{
+        bigNum = num2;
+        smallNum = num1;
+    }
+    for(let i = smallNum; i <= bigNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    /*if(year % 400 == 0)return true; //My solution
+    if(year % 100 == 0)return false;
+    if(year % 4 == 0)return true;
+    return false;*/
+    return year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0); //one-liner solution
 };
 
 // Do not edit below this line
